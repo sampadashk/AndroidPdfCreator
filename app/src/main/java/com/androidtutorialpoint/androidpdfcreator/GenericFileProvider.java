425a2270f612981b741b449ec85b9b86508ab368
Sampada <shukla.sampada@gmail.com>
2018-05-06 22:51:23 +0800

    PdfCreator

---
+++ app/src/main/java/com/androidtutorialpoint/androidpdfcreator/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package com.androidtutorialpoint.androidpdfcreator;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by KV on 6/2/18.
+ */
+
+public class GenericFileProvider extends FileProvider {
+}
